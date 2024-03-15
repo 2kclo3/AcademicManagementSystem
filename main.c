6c002fa86c042b08a7c61d3980cd23ba2cca2091
张久毅 <2231924086@qq.com>
2024-03-15 17:57:25 +0800

    初步就是这样的定义

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 #include "main.h"
 
 int main(void) {
-	showMenu("测试标题", 4, "增加", "修改", "查找", "删除");
-
+	//showMenu("测试标题", 4, "增加", "修改", "查找", "删除");
+	char str[20];
+	getText(str);
+	puts(str);
 	return 0;
 }
