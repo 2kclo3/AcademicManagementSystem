455a1a8cd46c2afd74bb15259d11fa216ac9fa8a
Lin <Lin@-14>
2024-03-20 16:42:28 +0800

    实现了对素质类项目链表的初始化

--- quality.c
+++ quality.c
@@ -3,7 +3,7 @@
 #include"io.h"
 #include"ui.h"
 #include<stdbool.h>
-//遗留问题：1.为每个学生的r/clist链表加入哨兵节点
+//遗留问题：1.为每个学生的r/clist链表加入哨兵节点 (OK
 //			2.searchR/Cnode()函数的实现
 
 Node* searchStuInQuality(List phead) { // 在素质类项目中搜索学生
@@ -25,6 +25,26 @@ Node* searchStuInQuality(List phead) { // 在素质类项目中搜索学生
 }
 
 
+void Initialize_Quality_List(List phead){// 初始化素质类项目的链表（为每个学生的r/clist链表加入哨兵节点）
+	List Ltmp = phead->next;
+	while (Ltmp != NULL) {
+		
+		Rnode* rhead = (Rnode*)malloc(sizeof(Rnode));
+		if (rhead == NULL)
+			return NULL;
+		rhead->rnext = NULL;
+		Ltmp->item.rlist = rhead;
+
+		Cnode* chead = (Cnode*)malloc(sizeof(Cnode));
+		if (chead == NULL)
+			return NULL;
+		chead->cnext = NULL;
+		Ltmp->item.clist = chead;
+
+		Ltmp = Ltmp->next;
+	}
+}
+
 
 bool addQuality_rlist(List* plist){
 	Node* ptmp = searchStu(plist);
@@ -38,33 +58,30 @@ bool addQuality_rlist(List* plist){
 	rnew->rnext = NULL;
 	printf("请输入你所需要申请添加的科研成果的相关信息:\n ");
 	
-	printf("1.论文名称（必填）：");
+	printf("1.论文名称（必填）：\n");
 	getText(rnew->research.paper_name);
 	
-	printf("2.期刊/会议名称（必填）：");
+	printf("2.期刊/会议名称（必填）：\n");
 	getText(rnew->research.journal_or_conference_name);
 	
-	printf("3.作者情况（仅需本人情况即可 必填）：");
+	printf("3.作者情况（仅需本人情况即可 必填）：\n");
 	getText(rnew->research.author);
 	
-	printf("4.发表时间（年月 必填）：");
+	printf("4.发表时间（年月 必填）：\n");
 	getText(rnew->research.date);
 	
-	printf("5.卷数（若无可不填）：");
+	printf("5.卷数（若无可不填）：\n");
 	getText(rnew->research.volume_num);
 	
-	printf("6.刊号（若无可不填）：");
+	printf("6.刊号（若无可不填）：\n");
 	getText(rnew->research.issue_num);
 	
-	printf("7.页码范围（必填）：");
+	printf("7.页码范围（必填）：\n");
 	getText(rnew->research.page);
 
 	rtmp->rnext = rnew;
 	return true;
 }
-
-
-
 bool addQuality_clist(List* plist) {
 	Node* ptmp = searchStu(plist);
 	Cnode* chead = ptmp->item.rlist;
@@ -77,22 +94,30 @@ bool addQuality_clist(List* plist) {
 	cnew->cnext = NULL;
 	printf("请输入你所需要申请添加的竞赛类获奖的相关信息:\n ");
 
-	printf("1.竞赛名称（必填）：");
+	printf("1.竞赛名称（必填）：\n");
 	getText(cnew->competition.competition_name);
 
-	printf("2.举办单位（必填）：");
+	printf("2.举办单位（必填）：\n");
 	getText(cnew->competition.organizer);
 
-	printf("3.获奖类别（必填）：");
+	printf("3.获奖类别（必填）：\n");
 	getText(cnew->competition.category);
 
-	printf("4.获奖时间（年月 必填）：");
+	printf("4.获奖时间（年月 必填）：\n");
 	getText(cnew->competition.date);
 
 	ctmp->cnext = cnew;
 	return true;
 }
 
+
+Rnode searchRnode(Rnode* rhead) {//找出待修改的素质类项目节点
+
+
+}
+Cnode searchCnode(Cnode* chead) {
+
+}
 
 
 void modifyQuality_rlist(List* plist) { // 修改素质类项目之科研成果
@@ -104,30 +129,44 @@ void modifyQuality_rlist(List* plist) { // 修改素质类项目之科研成果
 		int option = getNumber(8);
 		switch (option) {
 		case 1:
+			printf("原来的的论文名称:%s", rmod.research.paper_name);
+			printf("\n请输入新的论文名称:");
 			getText(rmod.research.paper_name);
 			break;
 
 		case 2:
+			printf("原来的期刊/会议名称:%s", rmod.research.journal_or_conference_name);
+			printf("\n请输入新的期刊/会议名称:");
 			getText(rmod.research.journal_or_conference_name);
 			break;
 
 		case 3:
+			printf("原来的作者情况:%s", rmod.research.author);
+			printf("\n请输入新的作者情况:");
 			getText(rmod.research.author);
 			break;
 
 		case 4:
+			printf("原来的发表时间:%s", rmod.research.date);
+			printf("\n请输入新的发表时间:");
 			getText(rmod.research.date);
 			break;
 
 		case 5:
+			printf("原来的卷数:%s", rmod.research.volume_num);
+			printf("\n请输入新的卷数:");
 			getText(rmod.research.volume_num);
 			break;
 
 		case 6:
+			printf("原来的刊号:%s", rmod.research.issue_num);
+			printf("\n请输入新的刊号:");
 			getText(rmod.research.issue_num);
 			break;
 
 		case 7:
+			printf("原来的页码范围:%s", rmod.research.page);
+			printf("\n请输入新的页码范围:");
 			getText(rmod.research.page);
 			break;
 
@@ -139,9 +178,6 @@ void modifyQuality_rlist(List* plist) { // 修改素质类项目之科研成果
 	}
 
 }
-
-
-
 void modifyQuality_clist(List* plist) { // 修改素质类项目之竞赛获奖
 	Node* ptmp = searchStu(plist);//找到待修改学生
 	Cnode* chead = ptmp->item.clist;
@@ -151,18 +187,26 @@ void modifyQuality_clist(List* plist) { // 修改素质类项目之竞赛获奖
 		int option = getNumber(5);
 		switch (option) {
 		case 1:
+			printf("原来的竞赛名称:%s", cmod.competition.competition_name);
+			printf("\n请输入新的竞赛名称:");
 			getText(cmod.competition.competition_name);
 			break;
 
 		case 2:
+			printf("原来的举办单位:%s", cmod.competition.organizer);
+			printf("\n请输入新的举办单位:");
 			getText(cmod.competition.organizer);
 			break;
 
 		case 3:
+			printf("原来的获奖类别:%s", cmod.competition.category);
+			printf("\n请输入新的获奖类别:");
 			getText(cmod.competition.category);
 			break;
 
 		case 4:
+			printf("原来的获奖时间:%s", cmod.competition.date);
+			printf("\n请输入新的获奖时间:");
 			getText(cmod.competition.date);
 			break;
 
--- quality.h
+++ quality.h
@@ -6,22 +6,31 @@
 #include <stdio.h>
 #include<stdbool.h>
 
+#include"student.h"
+
 
 void showAllQuality(); // 显示所有有素质类项目的学生
 
 
-Node searchStuInQuality(List); // 在素质类项目中搜索学生
+Node* searchStuInQuality(List); // 在素质类项目中搜索学生
+
+
+void Initialize_Quality_List(List);// 初始化素质类项目的链表（为每个学生的r/clist链表加入哨兵节点）
 
 
 bool addQuality_rlist(List*);// 添加素质类项目
 bool addQuality_clist(List*);
 
 
-void modifyQuality(List*);// 修改素质类项目
-
-
-Rnode searchRnode(Rnode*);//找出待修改的素质类项目节点
+Rnode searchRnode(Rnode*);// 找出此学生待修改的素质类项目节点
 Cnode searchCnode(Cnode*);
+
+
+void modifyQuality_rlist(List* plist);// 修改素质类项目
+void modifyQuality_clist(List* plist);
+
+
+
 
 
 bool deleteQuality(); // 删除素质类项目
