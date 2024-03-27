1e27906b2551ab053dad5eabd8f1388ec727803b
Tianyi Liu <hi2kclo3@outlook.com>
2024-03-27 22:43:50 +0800

    实现读取学生和课程函数

--- Liuty.c
+++ Liuty.c
@@ -1,138 +1,188 @@
 #pragma warning(disable:4996)
 
 #include "main.h"
-#define FILE_NAME ".\\data\\test.dat"
+#define STU_FILE ".\\data\\Student.txt"
+#define CRS_FILE ".\\data\\Course.txt"
 
 
-int main0111(void){ 
+int main9999(void){ 
+
 	
-	//页面显示
-	char* current_page = show_page("hello"); //当前页：hello
-	int command; // 指令
-	int is_teacher;
-	while (1) {
 
-		// 欢迎页
-		if (!strcmp(current_page, "hello")) {
-			command = getNumber(1); // 接收数字指令
-			if (command == 0) {
-				exit(0);
-			}
-			else if (command == 1) {
-				current_page = show_page("login");
-			}
+	Cpnode  crs = readCrs(CRS_FILE);
+
+	Cpnode Crscrt = crs->next;
+	while (Crscrt != NULL) {
+		printf("%d\n", Crscrt->cnum);
+		Spnode stu = Crscrt->sphead->next;
+		while (stu != NULL) {
+			printf("%s\n", stu->sname);
+			stu = stu->next;
 		}
+		Crscrt = Crscrt->next;
+		printf("\n");
 
-		// 登录页
-		else if (!strcmp(current_page, "login")) {
-			command = getNumber(3);
-			if (command == 0) {
-				current_page = show_page("hello");
-			}
-			else if (command == 1) {
-				is_teacher = 1;
-				current_page = show_page("menu");
-			}
-			else if (command == 2) {
-				current_page = show_page("menu");
-			}
-			else if (command == 3) {
-				current_page = show_page("menu");
-			}
-			//system("pause"); 
-			//登录先留着  
-			// //TODO
-		}
-
-		// 菜单页
-		else if (!strcmp(current_page, "menu")) {
-			command = getNumber(5); // 接收数字指令
-			if (command == 0) {
-				current_page = show_page("login");
-			}
-			else if (command == 1) {
-				//List* list = read(FILE_NAME);
-				////show_sheet(list, 1);
-				//current_page = "sheet";
-				current_page = show_page("allStu");
-			}
-			else if (command == 2) {
-
-				current_page = show_page("allCrs");
-			}
-			else if (command == 3) {
-				current_page = show_page("allQuality");
-			}
-			else if (command == 4) {
-				current_page = show_page("changeMajor");
-			}
-			else if (command == 5) {
-				current_page = show_page("settings");
-			}
-		}
-
-		//所有学生信息页面
-		else if (!strcmp(current_page, "allStu")) {
-
-		}
-
-		//所有课程信息页面
-		else if (!strcmp(current_page, "allCrs")) {
-			//showMenu("请输入课程号", 0);
-			//int cnum = getNumber(99999);
-			//showMenu("请输入学年", 0);
-			//int cgrade = getNumber(3000);
-
-			//searchCrs();
-
-
-
-		}
-
-		//总体素质类信息信息页面
-		else if (!strcmp(current_page, "allQuality")) {
-
-		}
-
-		//转专业管理页面
-		else if (!strcmp(current_page, "changeMajor")) {
-
-		}
-
-		// 表格页
-		else if (!strcmp(current_page, "sheet")) {
-			command = getNumber(4); // 接收数字指令
-			if (command == 0) {
-				current_page = show_page("menu");
-			}
-			else if (command == 1) {
-			}
-			else if (command == 2) {
-			}
-			else if (command == 3) {
-			}
-			else if (command == 4) {
-			}
-		}
-
-		// 设置页
-		else if (!strcmp(current_page, "settings")) {
-			command = getNumber(3); // 接收数字指令
-			if (command == 0) {
-				current_page = show_page("menu");
-			}
-		}
-
-		// 某页
-		else if (!strcmp(current_page, "")) {
-			command = getNumber(1); // 接收数字指令
-		}
-
-		// 不存在的页面
-		else {
-			break;
-		}
 	}
+
+
+	List stu = readStu(STU_FILE);
+
+	List Stucrt = stu->next;
+	while (Stucrt != NULL) {
+		printf("%s\n", Stucrt->item.data.name);
+		Crsnode* stu = Stucrt->item.crslist->crs_next;
+		while (stu != NULL) {
+			printf("%s\n", stu->score.course_name);
+			stu = stu->crs_next;
+		}
+		Stucrt = Stucrt->next;
+		printf("\n");
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	////页面显示
+	//char* current_page = show_page("hello"); //当前页：hello
+	//int command; // 指令
+	//int is_teacher;
+	//while (1) {
+
+	//	// 欢迎页
+	//	if (!strcmp(current_page, "hello")) {
+	//		command = getNumber(1); // 接收数字指令
+	//		if (command == 0) {
+	//			exit(0);
+	//		}
+	//		else if (command == 1) {
+	//			current_page = show_page("login");
+	//		}
+	//	}
+
+	//	// 登录页
+	//	else if (!strcmp(current_page, "login")) {
+	//		command = getNumber(3);
+	//		if (command == 0) {
+	//			current_page = show_page("hello");
+	//		}
+	//		else if (command == 1) {
+	//			is_teacher = 1;
+	//			current_page = show_page("menu");
+	//		}
+	//		else if (command == 2) {
+	//			current_page = show_page("menu");
+	//		}
+	//		else if (command == 3) {
+	//			current_page = show_page("menu");
+	//		}
+	//		//system("pause"); 
+	//		//登录先留着  
+	//		// //TODO
+	//	}
+
+	//	// 菜单页
+	//	else if (!strcmp(current_page, "menu")) {
+	//		command = getNumber(5); // 接收数字指令
+	//		if (command == 0) {
+	//			current_page = show_page("login");
+	//		}
+	//		else if (command == 1) {
+	//			//List* list = read(FILE_NAME);
+	//			////show_sheet(list, 1);
+	//			//current_page = "sheet";
+	//			current_page = show_page("allStu");
+	//		}
+	//		else if (command == 2) {
+
+	//			current_page = show_page("allCrs");
+	//		}
+	//		else if (command == 3) {
+	//			current_page = show_page("allQuality");
+	//		}
+	//		else if (command == 4) {
+	//			current_page = show_page("changeMajor");
+	//		}
+	//		else if (command == 5) {
+	//			current_page = show_page("settings");
+	//		}
+	//	}
+
+	//	//所有学生信息页面
+	//	else if (!strcmp(current_page, "allStu")) {
+
+	//	}
+
+	//	//所有课程信息页面
+	//	else if (!strcmp(current_page, "allCrs")) {
+	//		//showMenu("请输入课程号", 0);
+	//		//int cnum = getNumber(99999);
+	//		//showMenu("请输入学年", 0);
+	//		//int cgrade = getNumber(3000);
+
+	//		//searchCrs();
+
+
+
+	//	}
+
+	//	//总体素质类信息信息页面
+	//	else if (!strcmp(current_page, "allQuality")) {
+
+	//	}
+
+	//	//转专业管理页面
+	//	else if (!strcmp(current_page, "changeMajor")) {
+
+	//	}
+
+	//	// 表格页
+	//	else if (!strcmp(current_page, "sheet")) {
+	//		command = getNumber(4); // 接收数字指令
+	//		if (command == 0) {
+	//			current_page = show_page("menu");
+	//		}
+	//		else if (command == 1) {
+	//		}
+	//		else if (command == 2) {
+	//		}
+	//		else if (command == 3) {
+	//		}
+	//		else if (command == 4) {
+	//		}
+	//	}
+
+	//	// 设置页
+	//	else if (!strcmp(current_page, "settings")) {
+	//		command = getNumber(3); // 接收数字指令
+	//		if (command == 0) {
+	//			current_page = show_page("menu");
+	//		}
+	//	}
+
+	//	// 某页
+	//	else if (!strcmp(current_page, "")) {
+	//		command = getNumber(1); // 接收数字指令
+	//	}
+
+	//	// 不存在的页面
+	//	else {
+	//		break;
+	//	}
+	//}
 
 	return 0;
 }
--- file.c
+++ file.c
@@ -1,20 +1,111 @@
 #pragma warning(disable:4996)
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 #include "file.h"
-#include "course.h"
 
 
 
-/*
-读取文件
-作用：读取数据链表文件，返回一个链表首地址
-参数：文件路径
-返回：读取文件后保存到内存里的链表的首地址
-*/
+
+List readStu(char* file_name) {
+	FILE* fp;
+	List StuList = (List)malloc(sizeof(Node));
+	StuList->next = NULL;
+	StuList->item.crslist = (Crsnode*)malloc(sizeof(Crsnode));
+	StuList->item.crslist->crs_next = NULL;
+
+	fp = fopen(file_name, "r");//读取文件
+	if (fp == NULL) {
+		printf("Read \"%s\" error, please check and reboot the system!", file_name);
+		exit(EXIT_FAILURE);
+	}//读取失败退出
+
+	List tnode = (List)malloc(sizeof(Node)); //为tnode申请内存(临时存放)
+	tnode->next = NULL;
+	Crsnode* tcrs = (Crsnode*)malloc(sizeof(Crsnode)); //为tcrs申请内存(临时存放)
+	tcrs->crs_next = NULL;
+
+	//TODO
+
+	if (tnode == NULL || tcrs == NULL) {
+		printf("error!");
+		exit(EXIT_FAILURE);
+	}// 分配失败
+
+	char line[512];
+	while (fgets(line, sizeof(line), fp) != NULL) {
+		if (line[0] == '\n') { // 跳过空行
+			continue;
+		}
+
+		if (sscanf(line, "%d %s %d %d %s %s",
+			&tnode->item.data.ID,
+			&tnode->item.data.name,
+			&tnode->item.data.gender,
+			&tnode->item.data.grade,
+			&tnode->item.data.college,
+			&tnode->item.data.major
+		) == 6) { // 读取学生信息
+
+			tnode->item.crslist = (Crsnode*)malloc(sizeof(Crsnode)); //为一个crslist申请内存(添加到链表中的crslist)
+			tnode->item.crslist->crs_next = NULL;
+
+			if (tnode->item.crslist == NULL) {
+				printf("error!");
+				exit(EXIT_FAILURE);
+			}// 分配失败
+
+			while (fgets(line, sizeof(line), fp) != NULL) {
+				if (line[0] == '\n') { // 跳过空行
+					break;
+				}
+				if (sscanf(line, "%s %s %lf %d %d %lf %lf",
+					&tcrs->score.course_id,
+					&tcrs->score.course_name,
+					&tcrs->score.score,
+					&tcrs->score.semester,
+					&tcrs->score.course_nature,
+					&tcrs->score.credit,
+					&tcrs->score.grid
+				) == 7) { // 读取学生信息
+
+					// 添加到链表
+					Crsnode* crsnode = (Crsnode*)malloc(sizeof(Crsnode)); //为crsnode申请内存(添加到链表中的crsnode)
+					if (crsnode == NULL) {
+						printf("error!");
+						exit(EXIT_FAILURE);
+					}// 分配失败
+					memcpy(crsnode, tcrs, sizeof(Crsnode));
+					Crsnode* ptmp = tnode->item.crslist;
+					while (ptmp->crs_next != NULL) {
+						ptmp = ptmp->crs_next;
+					}
+					ptmp->crs_next = crsnode;
+
+
+				}
+			}
+
+			// 添加到链表
+			List Coursenode = (List)malloc(sizeof(Node)); //为Coursenode申请内存(添加到链表中的Coursenode)
+			if (Coursenode == NULL) {
+				printf("error!");
+				exit(EXIT_FAILURE);
+			}// 分配失败
+			memcpy(Coursenode, tnode, sizeof(Node));
+			List ptmp = StuList;
+			while (ptmp->next != NULL) {
+				ptmp = ptmp->next;
+			}
+			ptmp->next = Coursenode;
+
+		}
+	}
+	free(tnode);
+	free(tcrs);
+	fclose(fp);
+	return StuList;
+
+}
+
 Cpnode readCrs(char* file_name) {
 	FILE* fp;
 	Cpnode CrsList = (Cpnode)malloc(sizeof(_Cnode));
@@ -28,11 +119,9 @@ Cpnode readCrs(char* file_name) {
 		exit(EXIT_FAILURE);
 	}//读取失败退出
 
-	Cpnode tcnode = (Cpnode)malloc(sizeof(_Cnode)); //为cnode申请内存
+	Cpnode tcnode = (Cpnode)malloc(sizeof(_Cnode)); //为tcnode申请内存(存放临时的Cpnode)
 	tcnode->next = NULL;
-	tcnode->sphead = (Spnode)malloc(sizeof(Snode));
-	tcnode->sphead->next = NULL;
-	Spnode tsnode = (Spnode)malloc(sizeof(Snode)); //为tsnode申请内存
+	Spnode tsnode = (Spnode)malloc(sizeof(Snode)); //为tsnode申请内存(存放临时的Spnode)
 	tsnode->next = NULL;
 	if (tcnode == NULL || tsnode == NULL) {
 		printf("error!");
@@ -54,7 +143,15 @@ Cpnode readCrs(char* file_name) {
 			&tcnode->averscore,
 			&tcnode->totGPA,
 			&tcnode->averGPA
-		) == 8) { // 读取学生信息
+		) == 8) { // 读取课程信息
+
+			tcnode->sphead = (Spnode)malloc(sizeof(Snode));//为一个sphead申请内存(添加到链表中的sphead)
+			tcnode->sphead->next = NULL;
+			if (tcnode->sphead == NULL) {
+				printf("error!");
+				exit(EXIT_FAILURE);
+			}// 分配失败
+
 			while (fgets(line, sizeof(line), fp) != NULL) {
 				if (line[0] == '\n') { // 跳过空行
 					break;
@@ -64,11 +161,11 @@ Cpnode readCrs(char* file_name) {
 					&tsnode->sname,
 					&tsnode->score,
 					&tsnode->GPA
-				) == 4) {
+				) == 4) { // 读取学生信息
 					tsnode->next = NULL;
 
 					// 添加到链表
-					Spnode snode = (Spnode)malloc(sizeof(Snode)); //为snode申请内存
+					Spnode snode = (Spnode)malloc(sizeof(Snode)); //为snode申请内存(添加到链表中的snode)
 					if (snode == NULL) {
 						printf("error!");
 						exit(EXIT_FAILURE);
@@ -85,7 +182,7 @@ Cpnode readCrs(char* file_name) {
 			}
 
 			// 添加到链表
-			Cpnode cnode = (Cpnode)malloc(sizeof(_Cnode)); //为cnode申请内存
+			Cpnode cnode = (Cpnode)malloc(sizeof(_Cnode)); //为cnode申请内存(添加到链表中的cnode)
 			if (cnode == NULL) {
 				printf("error!");
 				exit(EXIT_FAILURE);
@@ -97,13 +194,26 @@ Cpnode readCrs(char* file_name) {
 			}
 			ptmp->next = cnode;
 
+
 		}
 	}
+	free(tcnode);
+	free(tsnode);
 	fclose(fp);
-	return &CrsList;
+	return CrsList;
 
 
 
+}
+
+bool saveStu(List StuList, char* file_name)
+{
+	return false;
+}
+
+bool saveCrs(Cpnode CrsList, char* file_name)
+{
+	return false;
 }
 
 
