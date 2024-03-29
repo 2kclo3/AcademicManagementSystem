a3da6a90e19ebb68594f9a8c83327046dfa30f6f
Tianyi Liu <hi2kclo3@outlook.com>
2024-03-29 20:36:33 +0800

    表格类实现！

--- Liuty.cpp
+++ Liuty.cpp
@@ -5,13 +5,10 @@
 #define CRS_FILE ".\\data\\Course.txt"
 
 
-int main99000(void){ 
+int mainAAAAAAAAAA(void){ 
 	setlocale(LC_ALL, ""); //使控制台支持宽字符输出
 
 
-
-	Cpnode  crs = readCrs(CRS_FILE);
-	List stu = readStu(STU_FILE);
 
 
 	// 初始化图形窗口
@@ -23,10 +20,10 @@ int main99000(void){
 
 	
 	
-	//testUI();
+	testUI();
 
 
-	loginUI();
+	//loginUI();
 
 
 
@@ -37,11 +34,6 @@ int main99000(void){
 	EndBatchDraw(); //结束批量绘图
 	closegraph(); // 关闭图形窗口
 	
-
-
-
-	saveStu(stu, STU_FILE);
-	saveCrs(crs, CRS_FILE);
 
 
 	
